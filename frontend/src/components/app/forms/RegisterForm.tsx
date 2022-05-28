c80a46b5e7fed8a8d0d569dca04492469a763b25
GaussianWonder <teo.virghi@yahoo.ro>
2022-05-28 17:56:17 +0300

    having debouncers to the register form input confuses the user

---
+++ frontend/src/components/app/forms/RegisterForm.tsx
@@ -0,0 +1,112 @@
+import { Component, createEffect, createResource, createSignal, onCleanup, ResourceFetcher, Show } from "solid-js";
+import auth, { AuthResponse, AuthUser } from "../../../store/auth";
+import Button from "../../button/Button";
+import LeftRightStrip from "../../strip/LeftRightStrip";
+import Input from "../input/Input";
+import { performLogin } from "./LoginForm";
+
+type RegisterBody = {
+  username: string;
+  password: string;
+}
+type RegisterFetcher = ResourceFetcher<RegisterBody, AuthResponse>;
+
+export const performRegister: RegisterFetcher = async (registerBody) => {
+  if (!registerBody.username || !registerBody.password)
+    return { access_token: '' };
+
+  const response = await fetch('http://localhost:3000/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: registerBody.username,
+        password: registerBody.password,
+      }),
+    });
+
+  if (!response.ok)
+    return { access_token: '' };
+
+  const data: AuthUser = await response.json();
+
+  if (!data || !data.id || !data.username)
+    return { access_token: '' };
+
+  const authResponse = await performLogin(registerBody, { value: undefined, refetching: false });
+
+  if (!authResponse || !authResponse.access_token)
+    return { access_token: '' };
+
+  return authResponse;
+};
+
+const RegisterForm: Component = () => {
+  const [username, setUsername] = createSignal<string>("");
+  const [password, setPassword] = createSignal<string>("");
+  const [, setToken] = auth;
+
+  const [registerSource, setRegisterSource] = createSignal<RegisterBody>({
+    username: '',
+    password: '',
+  });
+
+  const fetchUsingRegisterBody = () => {
+    setRegisterSource({
+      username: username(),
+      password: password(),
+    });
+  };
+
+  const [data] = createResource(registerSource, performRegister, {
+    deferStream: true,
+  });
+
+  createEffect(() => {
+    const authResponse = data();
+    if (authResponse && !!authResponse.access_token) setToken(authResponse.access_token);
+  });
+
+  return (
+    <div px-1 max-w-lg>
+      <Input
+        type="text"
+        name="username"
+        label="Username"
+        onInput={(e) => setUsername(e.target.value)}
+      />
+
+      <Input
+        type="password"
+        name="password"
+        label="Password"
+        onInput={(e) => setPassword(e.target.value)}
+      />
+
+      <LeftRightStrip
+        left={
+          <>
+            <Show when={data.loading}>
+              <div i-bx-grid-small animate-rotate-in w-6 h-6 animate-alternate animate-ease-in-out />
+            </Show>
+            <Show when={data.error}>
+              <span text-red-500>Errors...</span>
+            </Show>
+          </>
+        }
+        right={
+          <Button
+            style="primary"
+            onClick={fetchUsingRegisterBody}
+            disabled={data.loading}
+          >
+            Register
+          </Button>
+        }
+      />
+    </div>
+  );
+};
+
+export default RegisterForm;
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,15 +1,65 @@
-import { Component } from 'solid-js';
+import { Component, createEffect, Show } from 'solid-js';
+import RegisterForm from '../components/app/forms/RegisterForm';
+import InformativeLabel from '../components/app/label/InformativeLabel';
+import Button from '../components/button/Button';
 import SidebarNav from '../components/nav/SidebarNav';
 import PageHeading from '../layouts/PageHeading';
 import SidebarLayout from '../layouts/SidebarLayout';
+import auth from '../store/auth';
 
 const RegisterPage: Component = () => {
+  const [authState, logout] = auth;
+
+  createEffect(() => {
+    console.log(authState());
+  });
+
+  const isLoggedIn = () => {
+    return !!authState().access_token;
+  };
+
+  const username = () => {
+    return isLoggedIn() ? authState().user.username : '...loading';
+  };
+
   return (
     <SidebarLayout sidebar={<SidebarNav />}>
       <PageHeading
         title={<h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Register</h2>}
+        subtitle={
+          <>
+            <Show when={isLoggedIn()}>
+              <InformativeLabel>
+                <div i-bx-user mr-1 /> Already logged in as&nbsp;
+                <span text-green-500>{ username() }</span>
+              </InformativeLabel>
+            </Show>
+            <InformativeLabel>
+              <div i-bx-log-in-circle mr-1 /> Register a&nbsp;
+              <span>
+                <span text-yellow-500>new&nbsp;</span>
+                account
+              </span>
+            </InformativeLabel>
+            <InformativeLabel>
+              <div i-bx-info-circle mr-1 /> A successful register automatically logs you in 
+            </InformativeLabel>
+          </>
+        }
+        actions={
+          <Show when={isLoggedIn()}>
+            <Button
+              style='danger'
+              onClick={() => logout()}
+            >
+              Logout
+            </Button>
+          </Show>
+        }
       >
-        Regsiter Form
+        <div>
+          <RegisterForm />
+        </div>
       </PageHeading>
     </SidebarLayout>
   );
